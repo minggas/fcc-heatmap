bbd7087445b70eca727e624f0b86da0a81b246ed
minggas <fasfora@gmail.com>
2018-08-25 03:04:03 -0300

    init

--- js/main.js
+++ js/main.js
@@ -1,171 +1,211 @@
-const container = document.querySelector(".container");
-console.dir(container);
+const end =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 
-let w = container.clientWidth - 100,
-  h = container.clientHeight - 110;
+const margin = {
+  top: 80,
+  right: 30,
+  bottom: 80,
+  left: 90
+};
+let w = 1520 - margin.left - margin.right,
+  h = 630 - margin.top - margin.bottom;
 
-//Mouse over element
-const overlay = d3
-  .select(".container")
-  .append("div")
-  .attr("class", "overlay")
-  .style("opacity", 0);
-//Tooltip with data information
-const tooltip = d3
-  .select(".container")
+function monthParse(month) {
+  const date = new Date(0);
+  date.setUTCMonth(month);
+  return d3.timeFormat("%B")(date);
+}
+
+//Tooltip
+let tooltip = d3
+  .select("body")
   .append("div")
   .attr("id", "tooltip")
   .style("opacity", 0);
 //Chart container
-const svgContainer = d3
-  .select(".container")
+let svgContainer = d3
+  .select("body")
   .append("svg")
   .attr("class", "chart")
-  .attr("width", "100%")
-  .attr("height", "100%");
+  .attr("width", w + margin.left + margin.right)
+  .attr("height", h + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 //Fetch the data
-d3.json(
-  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
-)
+d3.json(end)
   .then(data => {
-    const yLabel = data.name.split(",")[0];
-    const dates = data.data.map(date => date[0]);
-    const values = data.data.map(val => val[1]);
-    const dataLength = data.data.length;
-    const minDate = new Date(d3.min(dates));
-    const maxDate = new Date(d3.max(dates));
-    const minValue = d3.min(values);
-    const maxValue = d3.max(values);
-    const barWidth = w / dataLength;
+    const baseTemp = data.baseTemperature;
+    const heatData = data.monthlyVariance;
+    const minTemp = data.baseTemperature + d3.min(heatData, d => d.variance);
+    const maxTemp = data.baseTemperature + d3.max(heatData, d => d.variance);
 
-    //Y Axis Label
-    const yText = svgContainer
-      .append("text")
-      .attr("class", "y-label")
-      .attr("transform", "rotate(-90)")
-      .attr("x", function(d, j) {
-        return h / 5 - h;
-      })
-      .attr("y", 80)
-      .text(yLabel);
-    //X Label informations
-    const xText = svgContainer
-      .append("text")
-      .attr("class", "x-label")
-      .attr("x", w)
-      .attr("y", h + 60)
-      .text(data.description.split("\n")[2].split("-")[0]);
-    svgContainer
-      .append("a")
-      .attr(
-        "href",
-        data.description
-          .split("\n")[2]
-          .split("-")[1]
-          .replace("(", "")
-          .replace(")", "")
+    //Color function
+    const legendColor = d3
+      .scaleThreshold()
+      .domain(
+        ((min, max, count) => {
+          let array = [];
+          let step = (max - min) / count;
+          let base = min;
+          for (let i = 1; i < count; i++) {
+            array.push(base + i * step);
+          }
+          return array;
+        })(minTemp, maxTemp, 12)
       )
-      .attr("target", "_blank")
-      .attr("class", "link")
-      .append("text")
-      .attr("class", "x-label")
-      .attr("x", w)
-      .attr("y", h + 80)
-      .text(data.description.split("\n")[2].split("-")[1]);
-    //Scale the chart with the time data
-    const xScale = d3
-      .scaleTime()
-      .domain([minDate, maxDate])
+      .range(d3.schemeRdYlBu[11].reverse());
+
+    //Scale the chart with the data
+    let xScale = d3
+      .scaleLinear()
+      .domain([d3.min(heatData, d => d.year), d3.max(heatData, d => d.year)])
       .range([0, w]);
 
-    const xAxis = d3.axisBottom().scale(xScale);
+    let yScale = d3
+      .scaleBand()
+      .domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+      .rangeRound([0, h]);
+
+    //Make the Axis
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+    const yAxis = d3.axisLeft(yScale).tickFormat(d => monthParse(d));
+
     //Put the X axis on the chart
-    const xAxisGroup = svgContainer
+    svgContainer
       .append("g")
-      .call(xAxis)
+      .attr("class", "x axis")
       .attr("id", "x-axis")
-      .attr("transform", `translate(60,${h})`);
-    //Scale the chart with the values data
-    const yScale = d3
-      .scaleLinear()
-      .domain([minValue, maxValue])
-      .range([(minValue / maxValue) * h, h]);
+      .attr("transform", "translate(0," + h + ")")
+      .call(xAxis)
+      .append("text")
+      .text("Year")
+      .attr("class", "x-axis-label")
+      .attr("x", w / 2)
+      .attr("y", 10)
+      .style("text-anchor", "end");
 
-    const scaledValues = values.map(function(item) {
-      return yScale(item);
-    });
-    //Create the Y axis scale
-    const yAxisScale = d3
-      .scaleLinear()
-      .domain([minValue, maxValue])
-      .range([h, (minValue / maxValue) * h]);
-
-    const yAxis = d3.axisLeft(yAxisScale);
     //Put the Y axis on the chart
-    const yAxisGroup = svgContainer
+    svgContainer
       .append("g")
-      .call(yAxis)
+      .attr("class", "y axis")
       .attr("id", "y-axis")
-      .attr("transform", "translate(60, 0)");
+      .call(yAxis);
+
     //Put the chart on the DOM
-    d3.select("svg")
+    svgContainer
+      .append("g")
+      .attr("class", "map")
+      .attr("transform", "translate(2,0)")
       .selectAll("rect")
-      .data(scaledValues)
+      .data(heatData)
       .enter()
       .append("rect")
-      .attr("data-date", (d, i) => dates[i])
-      .attr("data-gdp", (d, i) => values[i])
-      .attr("class", "bar")
-      .attr("x", (d, i) => i * barWidth)
-      .attr("y", d => h - d)
-      .attr("width", barWidth)
-      .attr("height", d => d)
-      .style("fill", "hsla(105, 30%, 50%, 0.5)")
-      .attr("transform", "translate(60, 0)")
-      //Make the tooltip appear and highligth the bar with the mouse over
-      .on("mouseover", (d, i) => {
-        overlay
-          .transition()
-          .duration(0)
-          .style("background-color", "hsla(105, 30%, 50%, 0.9)")
-          .style("height", `${d}px`)
-          .style("width", `${barWidth}px`)
-          .style("opacity", 0.9)
-          .style("left", `${i * barWidth + 15}px`)
-          .style("top", `${h - d + 13}px`)
-          .style("transform", "translateX(60px)");
-        tooltip
-          .transition()
-          .duration(200)
-          .style("opacity", 0.9);
-        tooltip
-          .html(
-            new Date(dates[i]).toLocaleDateString("en", {
-              year: "numeric",
-              month: "long"
-            }) +
-              "<br>" +
-              "$" +
-              values[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,") +
-              " Billion"
-          )
-          .attr("data-date", dates[i])
-          .style("left", `${i * barWidth - 40}px`)
-          .style("top", `${h - d - 45}px`)
-          .style("transform", "translateX(60px)");
-      })
-      .on("mouseout", function(d) {
-        overlay
-          .transition()
-          .duration(200)
-          .style("opacity", 0);
-        tooltip
-          .transition()
-          .duration(200)
-          .style("opacity", 0);
-      });
+      .attr("class", "cell")
+      .attr("data-month", d => d.month)
+      .attr("data-temp", d => d.variance)
+      .attr("data-year", d => d.year)
+      .attr("x", d => xScale(d.year))
+      .attr("y", d => yScale(d.month))
+      .attr("width", w / 262)
+      .attr("height", h / 12)
+      .style("fill", d => legendColor(baseTemp + d.variance))
+      //Make the tooltip show data with the mouse over
+      .on("mouseover", handleMouseOver)
+      .on("mouseout", handleMouseOut);
+    //Add Title and Subtitle
+    svgContainer
+      .append("text")
+      .attr("id", "title")
+      .attr("x", w / 2)
+      .attr("y", -margin.top / 2)
+      .attr("text-anchor", "middle")
+      .style("font-size", "2rem")
+      .text("Monthly Global Land-Surface Temperature");
+
+    svgContainer
+      .append("text")
+      .attr("x", w / 2)
+      .attr("y", -margin.top / 2 + 25)
+      .attr("id", "description")
+      .attr("text-anchor", "middle")
+      .style("font-size", "1rem")
+      .text(
+        `${d3.min(heatData, d => d.year)} - ${d3.max(
+          heatData,
+          d => d.year
+        )}: base temperature ${baseTemp}℃`
+      );
+
+    //Add the Legend to the Chart
+
+    var legendX = d3
+      .scaleQuantile()
+      .domain(
+        ((min, max, count) => {
+          let array = [];
+          let step = (max - min) / count;
+          let base = min;
+          for (let i = 1; i < count; i++) {
+            array.push(base + i * step);
+          }
+          return array;
+        })(minTemp, maxTemp, 11)
+      )
+      .range([0, 30, 60, 90, 120, 150, 180, 210, 240, 270]);
+
+    var legendXAxis = d3
+      .axisBottom(legendX)
+      .ticks(10)
+      .tickFormat(d3.format(".1f"));
+
+    let legend = svgContainer
+      .append("g")
+      .attr("id", "legend")
+      .attr("transform", "translate(" + 5 + "," + 500 + ")");
+
+    legend
+      .selectAll("rect")
+      .data(legendColor.domain())
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => 0 + i * 30)
+      .attr("width", 30)
+      .attr("height", 30)
+      .style("fill", (d, i) => d3.schemeRdYlBu[11][i])
+      .style("stroke", "black");
+
+    legend
+      .append("g")
+      .attr("class", "y axis")
+      .attr("id", "y-axis")
+      .attr("padding", 30)
+      .call(legendXAxis);
   })
   .catch(err => {
     alert(err);
   });
+
+function handleMouseOver(d, i) {
+  console.log(d.month);
+  tooltip
+    .transition()
+    .duration(200)
+    .style("opacity", 0.8);
+  tooltip
+    .html(
+      `${monthParse(d.month)} - ${d.year}<br/>${(8.66 + d.variance).toFixed(
+        2
+      )}℃<br/>${d.variance}`
+    )
+    .attr("data-year", d.year)
+    .style("left", d3.event.pageX + 15 + "px")
+    .style("top", d3.event.pageY - 5 + "px");
+}
+
+function handleMouseOut(d) {
+  tooltip
+    .transition()
+    .duration(200)
+    .style("opacity", 0);
+}
